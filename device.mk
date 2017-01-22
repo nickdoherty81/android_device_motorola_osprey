0c9b6ee85794092f3488ad8e62790c9cd5c51c8a
nickd81 <nickdoh81@gmail.com>
2017-01-22 01:30:40 -0500

    Use a smaller boot animation for our smaller screen
    Change-Id: I98c87f53c2117cae7dc8c9752d9701cda07d8013

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Use specific resolution for bootanimation
+SMALL_BOOTANIMATION_SIZE := 720x720
+
 # Custom APN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/apns-conf-vmob.xml:system/etc/apns-conf-vmob.xml \
